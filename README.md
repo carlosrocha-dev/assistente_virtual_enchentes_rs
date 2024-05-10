70cd553b5c7d2dfc1caf4b6e2184b6fbd39900e2
Carlos Rocha <carlos.rocha01@gmail.com>
2024-05-10 20:06:40 -0300

    update

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 ## Descrição
 
 Este projeto visa fornecer um assistente virtual projetado para oferecer suporte e informações cruciais para pessoas afetadas por enchentes no Rio Grande do Sul. Utilizando tecnologias de inteligência artificial através da API GeminAI da Google, o assistente é capaz de gerar respostas contextualizadas e pertinentes para auxiliar vítimas durante e após desastres naturais.
+
+
+## Exemplos de perguntas que você pode fazer ao assistente virtual:
+
+- Não consigo encontrar minha família, pode me ajudar?
+- Onde posso encontrar abrigo temporário?
+- Como posso obter informações sobre as condições locais?
+- O que devo fazer para me preparar para uma enchente?
+- Como posso acessar recursos como bancos de alimentos?
+- O que devo fazer para manter minha segurança durante uma enchente?
+- Como posso obter apoio emocional durante este período desafiador?
+- O que devo fazer para me recuperar após uma enchente?
+- Como posso ajudar outras pessoas afetadas pelas enchentes?
+
--- src/main.py
+++ src/main.py
@@ -1,56 +1,39 @@
 import google.generativeai as ai
 
 # Configure credentials
-API_KEY = 'YOUR API KEY'
-ai.configure(API_KEY)
+API_KEY = 'AIzaSyBQYNykGhYNcvzjKatX7MBgmE6wS1kw-Og'
+ai.configure(api_key=API_KEY)
 
 # Settings for the chatbot generation
 generation_config = {
-	'candidate': 1, # Number of candidates to generate
-	'max_tokens': 100, # Maximum number of tokens to generate
-	'temperature': 0.5, # Temperature for sampling
+    'temperature': 0.5,  # Temperature for sampling
 }
 
-# Safety settings to filter out inappropriate content
+# Safety settings to filter out inappropriate contents
 safety_settings = {
-	'HARASSMENT': 'BLOCK_NONE',
-	'HATE': 'BLOCK_NONE',
-	'SEXUAL': 'BLOCK_NONE',
-	'DANGEROUS': 'BLOCK_NONE',
+    'HARASSMENT': 'BLOCK_NONE',
+    'HATE': 'BLOCK_NONE',
+    'SEXUAL': 'BLOCK_NONE',
+    'DANGEROUS': 'BLOCK_NONE',
 }
 
 # Context for the chatbot
-context = """
-Este assistente virtual está projetado especificamente para oferecer suporte e informações vitais para indivíduos afetados pelas recentes enchentes no Rio Grande do Sul. Através deste serviço, você pode receber orientações sobre como se preparar para situações de emergência, o que fazer durante uma enchente para manter sua segurança, e como gerenciar a recuperação após o evento. Este assistente está equipado para ajudar com informações atualizadas sobre as condições locais, localização de abrigos temporários, acesso a recursos como bancos de alimentos e apoio emocional. Nosso objetivo é fornecer uma ferramenta acessível e útil para ajudar você a navegar por este período desafiador com informação e suporte confiáveis.
-"""
+context = "Este assistente virtual está projetado especificamente para oferecer suporte e informações vitais para indivíduos afetados pelas recentes enchentes no Rio Grande do Sul le outros desastres naturais que tornam as pessoas vulneraveis e com risco de morte. Através deste serviço, você pode receber orientações sobre como se preparar para situações de emergência, o que fazer durante uma enchente para manter sua segurança, e como gerenciar a recuperação após o evento. Este assistente está equipado para ajudar com informações atualizadas sobre as condições locais, localização de abrigos temporários, acesso a recursos como bancos de alimentos e apoio emocional. Nosso objetivo é fornecer uma ferramenta acessível e útil para ajudar você a navegar por este período desafiador com informação e suporte confiáveis."
 
 # Initialize the chatbot with model, context and GenAI client
 model = ai.GenerativeModel(model_name='gemini-1.0-pro',
-							generation_config=generation_config,
-							safety_settings=safety_settings,
-							context=context,
-							)
-
-genai_client = ai.Client(api_key=API_KEY)
+                           generation_config=generation_config,
+                           safety_settings=safety_settings)
 
 # Start the chatbot
 chat = model.start_chat(history=[])
 
-print("Olá! Lamento muito pelo que você está passando devido às enchentes no Rio Grande do Sul. Estou aqui para ajudar no que eu puder. Você precisa de abrigo, alimentação, informações ou outro tipo de auxílio?\n")
+prompt = input("\n\033[1;32mComo posso ajudá-lo(a) hoje? Estou aqui para oferecer suporte. Por favor, conte-me o que você precisa.\033[0m\n\n")
 
-while True:
-    user_input = input("O que você precisa? (abrigo, alimentação, etc. ou digite 'ajuda' para opções):\n").lower()
-    if user_input == "end":
-        break
+while prompt != "sair":
+	response = chat.send_message(context + prompt)
+	print("\033[1;32mAssistente: \033[0m\n", response.text, '\n')
+	prompt = input("\033[1;32mPosso ajudar com mais alguma coisa? \n(Para encerrar essa conversa digite `sair`)\033[0m\n")
 
-    # Generate a response from GenAI based on the user's input, including the context
-    response = genai_client.generate(
-        prompt=context + "\n\nUsuário: " + user_input + "\nAssistente:",
-        max_tokens=150
-    )
-
-    print("Assistente: ", response.text, '\n')
-
-    continue_prompt = input("Posso ajudar com mais alguma coisa? (sim/não)\n").lower()
-    if continue_prompt != "sim":
-        break
+if prompt == "sair":
+	print("\033[1;32mObrigado por usar o assistente virtual. Fique seguro e cuide-se!\033[0m\n")
